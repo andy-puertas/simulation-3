a0b0a04b3931bc4b6eba43b58ade7450dd5f06bd
Andy Puertas <andy.puertas08@gmail.com>
2018-06-08 14:56:52 -0600

    login and register work finally, starting redux

--- helo/server/index.js
+++ helo/server/index.js
@@ -1,20 +1,66 @@
 require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
-const passport = require('passport')
+//const passport = require('passport')
 const bodyParser = require('body-parser');
-const hc = require('./controller.js');
+//const hc = require('./controller.js');
 const massive = require('massive');
-const ctrl = require('../server/controller')
+//const ctrl = require('../server/controller')
 const app = express();
 
 const {
     SERVER_PORT,
-    CONNECTION_STRING
+    CONNECTION_STRING,
+    SESSION_SECRET
 } = process.env;
 
 
 app.use( bodyParser.json() );
+
+app.use(
+  session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+  })
+);
+
+app.get('/api/users', (req, res) => {
+  const db = req.app.get('db');
+        
+  db.show_users()
+  .then( users => res.status(200).send( users ) )
+  .catch( () => res.status(500).send('error') );
+})
+
+app.post('/api/register', (req, res) => {
+  const db = req.app.get('db');
+  const { username, password } = req.body;
+  const profile_pic = `https://robohash.org/${username}.png`;
+
+  db.create_user([username, password, profile_pic]).then((dbRes) => {
+    const { id, username, profile_pic } = dbRes[0];
+    req.session.userid = username;
+    res.status(200).send(id, username, profile_pic)
+    .catch(() => res.status(500).send('error') ) ;
+  });
+});
+
+app.post('/api/login', (req, res) => {
+  const db = req.app.get('db');
+  const { username, password } = req.body;
+  db.login_user([username]).then((dbResp) => {
+    const { id, profile_pic } = dbResp[0];
+    if (dbResp[0].username === username && dbResp[0].password === password) {
+      const username = dbResp[0].username;
+      req.session.userid = username;
+      res.status(200).send({ id, username, profile_pic });
+      // res.status(200).send('You logged in!');
+    } else {
+      res.sendStatus(404).send('lmao wrong');
+    }
+  });
+});
 
 
 massive(process.env.CONNECTION_STRING)
--- helo/src/components/Auth/Auth.js
+++ helo/src/components/Auth/Auth.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
-//import axios from 'axios';
-import './Auth.css'
+//import { Link } from 'react-router-dom'
+import axios from 'axios';
+import './Auth.css';
 
 export default class Auth extends Component {
     constructor(){
@@ -9,25 +10,62 @@ export default class Auth extends Component {
             username: '',
             password: ''
         }
+        this.handleUsername = this.handleUsername.bind( this )
+        this.handlePassword = this.handlePassword.bind( this )
+        this.registerUser = this.registerUser.bind( this )
+        this.loginUser = this.loginUser.bind( this )
+    }
+
+    handleUsername(input) {
+        this.setState({
+            username: input
+        })
+    }
+
+    handlePassword(input) {
+        this.setState({
+            password: input
+        })
+    }
+
+    registerUser() {
+        const {username, password} = this.state;
+        axios.post('/api/register', {username, password})
+        .then( res => {
+            this.props.history.push('/dashboard');
+        })
+    }
+
+    loginUser() {
+        const {username, password} = this.state;
+        axios.post('/api/login', {username, password})
+        .then( res => {
+            this.props.history.push('/dashboard');
+        })
     }
 
     render() {
+        console.log(this.state)
         return (
             <div className='Auth'>
                 <h1>Helo</h1>
 
-                Username: <input />
+                <p>Username:</p> 
+                <input onChange={(e) => {this.handleUsername(e.target.value)}} type='text'/>
                 <br />
                 <br />
 
-                Password: <input />
+                <p>Password:</p>
+                <input onChange={(e) => {this.handlePassword(e.target.value)}} type='password'/>
                 <br />
                 <br />
 
-                <button>Login</button>
-                <button>Register</button>
-            
+                <button onClick={this.loginUser}>Login</button>
+
+                <button onClick={this.registerUser}>Register</button>
+                
             </div>
         )
     }
 }
+                
